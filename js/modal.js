eafdccc9d405319a1fb30569137b4dd6363f4237
Shubham Arora <work.shubhamarora@gmail.com>
2019-08-04 18:42:15 +0530

    Added more screens

--- js/modal.js
+++ js/modal.js
@@ -3,22 +3,33 @@ var modal 				= document.getElementById('myModal');
 var moreScreensWatcho	= document.getElementById("more-screens-watcho");
 var moreScreensNonton = document.getElementById("more-screens-nonton");
 var moreScreensDialog 		= document.getElementById("more-screens-dialog");
+var moreScreensHalanx		= document.getElementById("more-screens-halanx");
+var moreScreensGamepaln 		= document.getElementById("more-screens-gameplan");
 var modalImg 			= document.getElementById("img01");
-
 
 moreScreensWatcho.onclick = function() {
     modal.style.display = "block";
-    modalImg.src = "img/index/product-watcho.jpg"
+    modalImg.src = "img/index/product-watcho.png"
 }
 
 moreScreensNonton.onclick = function() {
     modal.style.display = "block";
-    modalImg.src = "img/index/product-nonton.jpg"
+    modalImg.src = "img/index/product-nonton.png"
 }
 
 moreScreensDialog.onclick = function() {
     modal.style.display = "block";
-    modalImg.src = "img/index/product-dialog.jpg"
+    modalImg.src = "img/index/product-dialog.png"
+}
+
+moreScreensHalanx.onclick = function() {
+	modal.style.display = "block";
+	modalImg.src = "img/index/product-halanx.png"
+}
+
+moreScreensGamepaln.onclick = function() {
+	modal.style.display = "block";
+	modalImg.src = "img/index/product-gameplan.png"
 }
 
 var span = document.getElementsByClassName("close")[0];
